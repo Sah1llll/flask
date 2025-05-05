cec09c06ed6e460c33cfc4cdf8386605103c933c
Sah1llll <sahilkhan969677@gmail.com>
2025-05-05 14:07:32 +0530

    project work

--- app.py
+++ app.py
@@ -13,4 +13,8 @@ def about():
 def login():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 app.run(debug=True, host='0.0.0.0')
