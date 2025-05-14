47a9d6a1395c453e1a3c9add993460f4c0610bb1
Sah1llll <sahilkhan969677@gmail.com>
2025-05-14 17:40:31 +0530

    kdhufr7

--- app.py
+++ app.py
@@ -17,4 +17,19 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/form', methods=["GET",'POST'])
+def form():
+    if request.method == 'POST':
+        from_ = request.form['from']
+        to_ = request.form['to']
+        people = request.form['people']
+
+        days = request.form['days']
+        budget = request.form['budget']
+        print(from_, to_, people, days, budget)
+
+        return render_template('form.html',from_ = from_, to_ = to_, people = people, days = days, budget = budget)
+    return render_template('form.html')
+        
+
 app.run(debug=True, host='0.0.0.0')
