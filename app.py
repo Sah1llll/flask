279cb9776c871629c5c34e70c2f1e1c278e55c2d
Sah1llll <sahilkhan969677@gmail.com>
2025-05-14 18:07:54 +0530

    change

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,12 +24,16 @@ def form():
         from_ = request.form['from']
         to_ = request.form['to']
         people = request.form['people']
-
-        days = request.form['days']
+        from_date = request.form['fromdate']
+        to_date = request.form['todate']
         budget = request.form['budget']
-        print(from_, to_, people, days, budget)
+        # Convert string date to datetime object
+        from_date = datetime.strptime(from_date, '%Y-%m-%d')
+        to_date = datetime.strptime(to_date, '%Y-%m-%d')
+        days = (to_date - from_date).days
+        print(from_, to_, people, budget, from_date, to_date, days)
 
-        return render_template('form.html',from_ = from_, to_ = to_, people = people, days = days, budget = budget)
+        return render_template('form.html',from_ = from_, to_ = to_, people = people, budget = budget, from_date = from_date, to_date = to_date)
     return render_template('form.html')
         
 
